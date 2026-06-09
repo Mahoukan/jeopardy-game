28cadc4456643366096473899491d3607ee5253a
Mahoukan <suriyakrishnans@gmail.com>
2026-06-09 20:25:26 +1200

    testing css version of button tap prevention

--- public/player.js
+++ public/player.js
@@ -417,20 +417,3 @@ setInterval(() => {
     socket.emit("heartbeat");
   }
 }, 30000);
-
-
-let lastTouchEnd = 0;
-
-document.addEventListener(
-  "touchend",
-  (event) => {
-    const now = Date.now();
-
-    if (now - lastTouchEnd <= 300) {
-      event.preventDefault();
-    }
-
-    lastTouchEnd = now;
-  },
-  { passive: false },
-);
